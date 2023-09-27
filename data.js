9f1c591f72786a1d44c827ca3b1eecffaccd2c81
Kayho-Rose <imandmov@gmail.com>
2023-09-27 16:19:54 +0300

    Day #1

---
+++ data.js
@@ -0,0 +1,29 @@
+const myProject = [
+    {
+        name: `Catapp `,
+        data: `27/09/2023`,
+        descryption: `The web application was created using Firebase. 
+                The task is to add and delete a list of products in a convenient way`,
+        githubUrl: `https://github.com/Kayho-Rose/Catapp`,
+        netlifyUrl: `https://catappmovchan.netlify.app`,
+        img: `/images/catapp.PNG`
+    },
+    {
+        name: `Twimba`,
+        data: `27/09/2023`,
+        descryption: `Attempting to create an X (Twitter) feed. Due to deploying to Netlify, 
+                    the images in the feed are no longer linked.But when displaying the code through Life Server, 
+                    everything is displayed as expected`,
+        githubUrl: `https://github.com/Kayho-Rose/Twimba_Re`,
+        netlifyUrl: `https://mytwimbamovchan.netlify.app`,
+        img: `/images/twimba.PNG`
+    },
+    {
+        name: `Grid site`,
+        data: `27*09/2023`,
+        descryption: `The blog site is made using CSS grid and responsible design`,
+        githubUrl: `https://github.com/Kayho-Rose/grid`,
+        netlifyUrl: `gridmovchan.netlify.app`,
+        img: `/images/grid.PNG`
+    }
+]
